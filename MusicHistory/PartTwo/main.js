3af7332d770f085b0ff3d9c9028dc8f761fdc5ef
Greg Korte <gregmkorte@gmail.com>
2015-08-03 11:49:10 -0500

    Moved PartOne files, added main.js file, added songs to beginning and end of song array.

---
+++ MusicHistory/PartTwo/main.js
@@ -0,0 +1,12 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+songs.unshift("Any Colour You Like by Pink Floyd on the album Dark Side of the Moon");
+songs.push("Dogs by Pink Floyd on the album Animals");
+
+console.log(songs)
