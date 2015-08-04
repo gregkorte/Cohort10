c18483994cea2c3292ad64f721484a03e1811f08
Greg Korte <gregmkorte@gmail.com>
2015-08-04 13:37:18 -0500

    Added script to main.js.

---
+++ JS101/PartTwo/main.js
@@ -0,0 +1,5 @@
+var header = document.getElementById('header');
+var section = document.getElementById('section');
+var footer = document.getElementById('footer');
+
+header.innerHTML = "<h1>JavaScript Exercises</h1>"
