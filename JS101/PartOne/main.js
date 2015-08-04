9f1c2161e6cf90234d3fe0d8c8091437b2b823b9
Greg Korte <gregmkorte@gmail.com>
2015-08-04 10:56:16 -0500

    Wrote conditional to output age statement.

--- JS101/PartOne/main.js
+++ JS101/PartOne/main.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 function hoursPerYear() {
 	return 24*365 + " hours";
 }
@@ -6,17 +8,28 @@ function minutesPerDecade() {
 	return 60*24*365*10 + " minutes";
 }
 
+function checkAge(diff) {
+	if(diff > 2500000000) {
+		$('#seconds-old').append("Dang, you are aging well...");
+	}
+	else {
+		$('#seconds-old').append("You're still a spring chicken, eh?");
+	}
+}
+
 function secondsOld() {
 	var input = Date.parse(prompt("Enter your birthday in the following format mm/dd/yyyy."));
 	var currentDate = new Date();
-	return (currentDate - input)/1000 + " seconds";
+	var diff = (currentDate - input)/1000;
+	checkAge(diff);
+	return "<br>" + diff + " seconds";
 }
 
-$(document).ready(function(){
 	var hrsYr = hoursPerYear();
 	$('#hours-in-year').append(hrsYr);
 	var minDec = minutesPerDecade();
 	$('#minutes-in-decade').append(minDec)
 	var secOld = secondsOld();
 	$('#seconds-old').append(secOld);
+
 })
