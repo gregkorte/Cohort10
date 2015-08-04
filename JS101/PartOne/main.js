ed69ae06492f994bf2d24ad3c062775716ac9c59
Greg Korte <gregmkorte@gmail.com>
2015-08-04 10:35:40 -0500

    Wrote secondsOld function.

--- JS101/PartOne/main.js
+++ JS101/PartOne/main.js
@@ -6,9 +6,17 @@ function minutesPerDecade() {
 	return 60*24*365*10 + " minutes";
 }
 
+function secondsOld() {
+	var input = Date.parse(prompt("Enter your birthday in the following format mm/dd/yyyy."));
+	var currentDate = new Date();
+	return (currentDate - input)/1000 + " seconds";
+}
+
 $(document).ready(function(){
 	var hrsYr = hoursPerYear();
 	$('#hours-in-year').append(hrsYr);
 	var minDec = minutesPerDecade();
 	$('#minutes-in-decade').append(minDec)
+	var secOld = secondsOld();
+	$('#seconds-old').append(secOld);
 })
