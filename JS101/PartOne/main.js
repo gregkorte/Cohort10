ea83251c5e56a01b2cb93658161ab7f65901751e
Greg Korte <gregmkorte@gmail.com>
2015-08-04 09:28:50 -0500

    Wrote minutes per decade function.

--- JS101/PartOne/main.js
+++ JS101/PartOne/main.js
@@ -1,9 +1,14 @@
 function hoursPerYear() {
-	return 24*365;
+	return 24*365 + " hours";
 }
 
+function minutesPerDecade() {
+	return 60*24*365*10 + " minutes";
+}
 
 $(document).ready(function(){
 	var hrsYr = hoursPerYear();
 	$('#hours-in-year').append(hrsYr);
+	var minDec = minutesPerDecade();
+	$('#minutes-in-decade').append(minDec)
 })
