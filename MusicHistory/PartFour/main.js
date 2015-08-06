17a489cda95a4c93a0fe1d1d4f7d8c8b3c6991ed
Greg Korte <gregmkorte@gmail.com>
2015-08-06 12:37:17 -0500

    Added songs2 json file, added more button, added more button functionality, handled more button errors.

--- MusicHistory/PartFour/main.js
+++ MusicHistory/PartFour/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+	var counter = 0;
 
-	var output = '';
 	$.ajax({
 		url: "songs.json"
 	}).done(function(data){
+		var output = '';
 		json = $.parseJSON(data)
 		for(var i = 0; i < json.songs.length; i++){
 			output += "<section><header class='song-title'><h1>"
@@ -16,11 +17,41 @@ $(document).ready(function(){
 			+ json.songs[i].year
 			+ "</li><li><button class='deletebtn'>x</button></li></ul></section>"
 		}
-		$('#content').append(output);
+		$('#content').prepend(output);
+	});
+
+	function getMore(){
+		$.ajax({
+			url: "songs2.json"
+		}).done(function(data){
+			var output = '';
+			json = $.parseJSON(data)
+			for(var i = 0; i < json.songs.length; i++){
+				output += "<section><header class='song-title'><h1>"
+				+ json.songs[i].title
+				+ "</h1></header><ul><li>"
+				+ json.songs[i].artist
+				+ "</li><li>"
+				+ json.songs[i].album
+				+ "</li><li>"
+				+ json.songs[i].year
+				+ "</li><li><button class='deletebtn'>x</button></li></ul></section>"
+			}
+			$('.more').before(output);
+		});
+	}
+
+	$('#content').on('click', '.more', function(){
+		counter++
+		if(counter > 1) {
+			return;
+		} else {
+			getMore();
+		}
 	});
 
 	$('#content').on('click', '.deletebtn', function(){
 			$(this).closest('section').remove();
-	})
+	});
 
 });
