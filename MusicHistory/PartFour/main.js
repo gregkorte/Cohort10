66dce36bccc87eec540e7df858c05b37721cb530
Greg Korte <gregmkorte@gmail.com>
2015-08-06 10:53:40 -0500

    Added delete button for each song, individual delete working.

--- MusicHistory/PartFour/main.js
+++ MusicHistory/PartFour/main.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
+
 	var output = '';
 	$.ajax({
 		url: "songs.json"
 	}).done(function(data){
 		json = $.parseJSON(data)
 		for(var i = 0; i < json.songs.length; i++){
-			output += "<section><header class='song-title'><h1>" 
-								+ json.songs[i].title
-								+ "</h1></header><ul><li>"
-								+ json.songs[i].artist
-								+ "</li><li>"
-								+ json.songs[i].album
-								+ "</li><li>"
-								+ json.songs[i].year
-								+ "</li></ul></section>"
+			output += "<section><header class='song-title'><h1>"
+			+ json.songs[i].title
+			+ "</h1></header><ul><li>"
+			+ json.songs[i].artist
+			+ "</li><li>"
+			+ json.songs[i].album
+			+ "</li><li>"
+			+ json.songs[i].year
+			+ "</li><li><button class='deletebtn'>x</button></li></ul></section>"
 		}
 		$('#content').append(output);
 	});
+
+	$('#content').on('click', '.deletebtn', function(){
+			$(this).closest('section').remove();
+	})
+
 });
