24095161a04d93225b974d359d33a4774d9686e3
Greg Korte <gregmkorte@gmail.com>
2015-08-08 07:14:46 -0500

    Set up main and language js shell files.

---
+++ JS103/Translation/farsi.js
@@ -0,0 +1,13 @@
+var Translation = (function(trans) {
+	var far = {"merry": "شاد", "christmas": "کریسمس", "and": "و", "happy": "خوشحال", "new": "جدید", "year": "سال"};
+
+  trans.translateToFarsi = function(input) {
+  	;
+	},
+  trans.getfar = function() {
+    ;
+  };
+
+  return trans;
+
+})(Translation);
