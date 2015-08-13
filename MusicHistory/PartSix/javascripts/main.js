dad45356535d7819151f869ba3d6302f4b1732b9
gregkorte <gryphionsroost@yahoo.com>
2015-08-13 15:18:46 -0500

    Updated gitignore, setting firebase data.

--- MusicHistory/PartSix/javascripts/main.js
+++ MusicHistory/PartSix/javascripts/main.js
@@ -1,17 +1,36 @@
 requirejs.config({
 	baseUrl: './javascripts',
 	paths: {
-		'jquery': '../bower_components/jquery/dist/jquery.min'
+		'jquery': '../bower_components/jquery/dist/jquery.min',
+		'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
+		'firebase': '../bower_components/firebase/firebase',
+		'hbs': '../bower_components/require-handlebars-plugin/hbs/handlebars'
+	},
+	shim: {
+		'bootstrap': ['jquery']
 	}
 });
 
 requirejs(
-  ["jquery", "dom-access", "populate-songs", "get-more-songs"],
-  function($, dom, pop, more) {
+  ["jquery", "bootstrap", "firebase", "hbs", "dom-access", "populate-songs", "get-more-songs"],
+  function($, boot, fire, hbs, dom, pop, more) {
+
+  var fb = new Firebase("https://nsscohort10.firebaseio.com/music-history");
 
   var initialSongs = [];
 	var output = dom.getInitOutput();
 	var counter = 0;
+
+	fb.set({
+		"songs": [
+        {
+            "title": "Astronomy Domine",
+            "artist": "Pink Floyd",
+            "album": "The Piper at the Gates of Dawn",
+            "year": "1967"
+        }
+      ]
+	})
 
 	pop.getInitSongs(function(data){
 	}, output);
