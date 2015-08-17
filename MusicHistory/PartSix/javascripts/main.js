9a2a7742b75de896fd4614dbe029b07959c65898
gregkorte <gryphionsroost@yahoo.com>
2015-08-17 09:34:26 -0500

    Added partial handlebars, require working on main.js.

--- MusicHistory/PartSix/javascripts/main.js
+++ MusicHistory/PartSix/javascripts/main.js
@@ -4,7 +4,7 @@ requirejs.config({
 		'jquery': '../bower_components/jquery/dist/jquery.min',
 		'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
 		'firebase': '../bower_components/firebase/firebase',
-		'hbs': '../bower_components/require-handlebars-plugin/hbs/handlebars'
+		'hbs': '../bower_components/require-handlebars-plugin/hbs'
 	},
 	shim: {
 		'bootstrap': ['jquery'],
@@ -16,17 +16,35 @@ requirejs.config({
 
 requirejs(
   ["jquery", "bootstrap", "firebase", "hbs", "dom-access", "populate-songs"],
-  function($, boot, fire, hbs, dom, pop) {
+  function($, boot, _fire, Handlebars, dom, pop) {
 
+	var fb = new Firebase("https://nsscohort10.firebaseio.com/music-history/");
 	var output = dom.getInitOutput();
 
+	var getHbs = function(arr, temp){
+		// console.log('3. getHbs running');
+		// console.log(arr);
+		// console.log(temp);
+		require(['hbs!../templates/' + temp], function(template){
+			console.log('Require loaded!');
+			console.log(arr);
+			var songList = template(arr);
+			console.log(songList);
+			output.html(songList);
+		});
+	};
+
 	pop.getInitSongs(function(data){
-	}, output);
+		fb.child('songs').on('value', function(snapshot) {
+		var songArray = [];
+		// console.log('2. Accessing fb');
+		songArray.push(snapshot.val());
+		getHbs(songArray, 'musicMain');
+		});
+	});
 
 	$('#content').on('click', '.deletebtn', function(){
 			$(this).closest('section').remove();
 	});
-
-	
 
 });
--- MusicHistory/PartSix/javascripts/populate-songs.js
+++ MusicHistory/PartSix/javascripts/populate-songs.js
@@ -1,23 +1,11 @@
 define(function(){
 	return{
-		getInitSongs: function(prepend, to){
+		getInitSongs: function(cb2main){
+			// console.log('1. getInitSongs running');
 			$.ajax({
 				url: "https://nsscohort10.firebaseio.com/music-history/"
 			}).done(function(data){
-			  var fb = new Firebase("https://nsscohort10.firebaseio.com/music-history/");
-				// var json = $.parseJSON(data);
-				fb.on("value", function(snapshot) {
-				  console.log(snapshot.val());
-				  var json = snapshot.val();
-				  prepend(json.songs);
-					var output = '';
-					for(var i = 0; i < json.songs.length; i++){
-					output += "<section><header class='song-title'><h1>" + json.songs[i].title + "</h1></header><ul><li>" + json.songs[i].artist + "</li><li>" + json.songs[i].album + "</li><li>" + json.songs[i].year + "</li><li><button class='deletebtn'>x</button></li></ul></section>";
-				}
-				to.prepend(output);
-				}, function (errorObject) {
-				  console.log("The read failed: " + errorObject.code);
-				});
+				cb2main.call(this, data);
 			});
 		}
 	};
