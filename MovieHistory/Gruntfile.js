ffaededfd99b1db720d65c1bdd4dd418055f88ee
gregkorte <gryphionsroost@yahoo.com>
2015-08-20 13:52:21 -0500

    Bolierplate installed.

---
+++ MovieHistory/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['./javascripts/**/*.js']
+    },
+    sass: {
+      dist: {
+        files: {
+          'styles/styles.css': 'sass/styles.scss'
+        }
+      }
+    },
+    watch: {
+      javascripts: {
+        files: ['./javascripts/**/*.js'],
+        tasks: ['jshint']
+      }
+    }
+  });
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.registerTask('default', ['jshint', 'sass', 'watch']);
+};
