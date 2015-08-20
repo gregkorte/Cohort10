4d665e86fbc22aebced05dae642646a6627bbe5c
gregkorte <gryphionsroost@yahoo.com>
2015-08-20 12:40:26 -0500

    Uninstalled jquery-ui.

--- JS201/javascripts/main.js
+++ JS201/javascripts/main.js
@@ -2,7 +2,6 @@ requirejs.config({
   baseUrl: './javascripts',
   paths: {
     'jquery': '../bower_components/jquery/dist/jquery.min',
-    'jquery-ui': '../bower_components/jquery-ui/jquery-ui.min',
     'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
     'firebase': '../bower_components/firebase/firebase',
     'hbs': '../bower_components/require-handlebars-plugin/hbs',
@@ -16,8 +15,8 @@ requirejs.config({
 });
 
 requirejs(
-  ["jquery", "jquery-ui", "bootstrap", "firebase", "hbs", "dom", "populate", "add", 'delete', 'templates'],
-  function($, ui, boot, _fire, Handlebars, dom, pop, add, del, hbs) {
+  ["jquery", "bootstrap", "firebase", "hbs", "dom", "populate", "add", 'delete', 'templates'],
+  function($, boot, _fire, Handlebars, dom, pop, add, del, hbs) {
 
   pop.getAllUsers();
 
