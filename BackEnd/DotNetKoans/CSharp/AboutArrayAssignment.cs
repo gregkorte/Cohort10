6bc253e288e3178df211a715c72fc4446cdcca9e
gregkorte <gryphionsroost@yahoo.com>
2015-10-20 10:27:36 -0500

    Completed AboutArrayAssignment Koans (17-19).

--- BackEnd/DotNetKoans/CSharp/AboutArrayAssignment.cs
+++ BackEnd/DotNetKoans/CSharp/AboutArrayAssignment.cs
@@ -21,7 +21,7 @@ namespace DotNetKoans.CSharp
             //Even though we don't specify types explicitly, the compiler
             //will pick one for us
             var name = "John";
-            Assert.Equal(typeof(FillMeIn), name.GetType());
+            Assert.Equal(typeof(string), name.GetType());
 
             //but only if it can. So this doesn't work
             //var array = null;
@@ -36,7 +36,7 @@ namespace DotNetKoans.CSharp
             //Even though we don't specify types explicitly, the compiler
             //will pick one for us
             var names = new[] { "John", "Smith" };
-            Assert.Equal(typeof(FillMeIn), names.GetType());
+            Assert.Equal(typeof(string[]), names.GetType());
 
             //but only if it can. So this doesn't work
             //var array = new[] { "John", 1 };
@@ -48,8 +48,8 @@ namespace DotNetKoans.CSharp
             //You can do multiple assignments on one line, but you 
             //still have to be explicit
             string firstName = "John", lastName = "Smith";
-            Assert.Equal(FILL_ME_IN, firstName);
-            Assert.Equal(FILL_ME_IN, lastName);
+            Assert.Equal("John", firstName);
+            Assert.Equal("Smith", lastName);
         }
     }
 }
