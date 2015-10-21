f3aa951b39fcc72b3f83d90bbfb3a18484bfebae
gregkorte <gryphionsroost@yahoo.com>
2015-10-21 11:02:37 -0500

    Completed AboutLambdas Koans (139-145).

--- BackEnd/DotNetKoans/CSharp/AboutLambdas.cs
+++ BackEnd/DotNetKoans/CSharp/AboutLambdas.cs
@@ -22,7 +22,7 @@ namespace DotNetKoans.CSharp
 														return x.ToString();
 													});
 
-			Assert.Equal(FILL_ME_IN, result);
+			Assert.Equal(new string[] { "1", "2", "3", "4" }, result);
 		}
 		[Koan(2)]
 		public void AnonymousMethodsCanAccessOuterVariables()
@@ -31,7 +31,7 @@ namespace DotNetKoans.CSharp
 			//In C# this is called accessing an Outer Variable. In other languages it is called closure. 
 			var numbers = new[] { 4, 5, 6, 7, 8, 9 };
 			int toFind = 7;
-			Assert.Equal(FILL_ME_IN, Array.FindIndex(numbers, delegate(int x)
+			Assert.Equal(3, Array.FindIndex(numbers, delegate(int x)
 													{
 														return x == toFind;
 													}));
@@ -50,7 +50,7 @@ namespace DotNetKoans.CSharp
 			}
 			var numbers = new[] { 4, 5, 6, 7, 8, 9 };
 			//toFind is not available here, yet criteria still works
-			Assert.Equal(FILL_ME_IN, Array.FindIndex(numbers, criteria));
+			Assert.Equal(3, Array.FindIndex(numbers, criteria));
 		}
 		[Koan(4)]
 		public void LambdaExpressionsAreShorthand()
@@ -71,7 +71,7 @@ namespace DotNetKoans.CSharp
 													{
 														return x.ToString(); 
 													});
-			Assert.Equal(FILL_ME_IN, anonymous);
+			Assert.Equal(new string[] { "1", "2", "3", "4" }, anonymous);
 			//The => pair is spoken as "going into". If you were talking about this 
 			//code with a peer, you would say "x going into..."
 		}
@@ -91,7 +91,7 @@ namespace DotNetKoans.CSharp
 													{
 														return x.ToString();
 													});
-			Assert.Equal(FILL_ME_IN, anonymous);
+			Assert.Equal(new string[] { "1", "2", "3", "4" }, anonymous);
 		}
 		[Koan(6)]
 		public void ParensNotNeededOnSingleParemeterLambdas()
@@ -107,7 +107,7 @@ namespace DotNetKoans.CSharp
 													{
 														return x.ToString();
 													});
-			Assert.Equal(FILL_ME_IN, anonymous);
+            Assert.Equal(new string[] { "1", "2", "3", "4" }, anonymous);
 		}
 		[Koan(7)]
 		public void BlockNotNeededOnSingleStatementLambdas()
@@ -119,7 +119,7 @@ namespace DotNetKoans.CSharp
 			});
 			var lambda = Array.ConvertAll(numbers, x => x.ToString());
 			//When you have only one statement, the curly brackets are not needed. What other two things are also missing?
-			Assert.Equal(FILL_ME_IN, anonymous);
+            Assert.Equal(new string[] { "1", "2", "3", "4" }, anonymous);
 		}
 	}
 }
