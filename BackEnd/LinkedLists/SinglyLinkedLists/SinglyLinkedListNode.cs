0e1edcdd580d9bf3f24dc04b1de653291c091349
gregkorte <gryphionsroost@yahoo.com>
2015-10-26 15:02:52 -0500

    Test 1 NodeConstructorStoresName working.

---
+++ BackEnd/LinkedLists/SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Stretch Goals: Using Generics, which would include implementing GetType() http://msdn.microsoft.com/en-us/library/system.object.gettype(v=vs.110).aspx
+namespace SinglyLinkedLists
+{
+    public class SinglyLinkedListNode : IComparable
+    {
+        // Used by the visualizer.  Do not change.
+        public static List<SinglyLinkedListNode> allNodes = new List<SinglyLinkedListNode>();
+
+        // READ: http://msdn.microsoft.com/en-us/library/aa287786(v=vs.71).aspx
+        private SinglyLinkedListNode next;
+        public SinglyLinkedListNode Next
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        private string value;
+        public string Value 
+        {
+            get { return value; }
+        }
+
+        public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
+        {
+            // This implementation is provided for your convenience.
+            return node1.CompareTo(node2) < 0;
+        }
+
+        public static bool operator >(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
+        {
+            // This implementation is provided for your convenience.
+            return node1.CompareTo(node2) > 0;
+        }
+
+        public SinglyLinkedListNode(string value)
+        {
+            this.value = value;
+
+            // Used by the visualizer:
+            allNodes.Add(this);
+        }
+
+        // READ: http://msdn.microsoft.com/en-us/library/system.icomparable.compareto.aspx
+        public int CompareTo(Object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsLast()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
