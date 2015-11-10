5a6858256273c695a6218e1630c740e20f0511f5
gregkorte <gryphionsroost@yahoo.com>
2015-11-10 11:17:26 -0600

    Created HelloWorld project with Program and Hello1 classes.

---
+++ BackEnd/HelloWorld/HelloWorld/Hello1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Hello1
+    {
+        public static string call1()
+        {
+            return "Hello, World the first";
+        }
+    }
+}
---
+++ BackEnd/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Hello1.call1());
+
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+        }
+    }
+}
