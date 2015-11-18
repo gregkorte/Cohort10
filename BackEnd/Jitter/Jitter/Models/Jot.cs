6a7821dedf4e0c9d55a5d15a6485566dc53c4469
gregkorte <gryphionsroost@yahoo.com>
2015-11-18 12:11:32 -0600

    Created initial tests and classes.

---
+++ BackEnd/Jitter/Jitter/Models/Jot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jitter.Models
+{
+    public class Jot
+    {
+        public int JotId { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public object Author { get; set; }
+
+        public string Picture { get; set; }
+    }
+}
